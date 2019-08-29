d0529d4e5ade07d1044c2d39a90b40a762c79a33
KevinBK <kevinbijoykurian@gmail.com>
2019-08-30 02:19:50 +0530

    Stage 16 complete

--- xfs-interface/ease.bat
+++ xfs-interface/ease.bat
@@ -1,6 +1,8 @@
-load --init $HOME/myexpos/expl/samples/odd.xsm
+load --init $HOME/myexpos/expl/samples/gcd.xsm
 load --os $HOME/myexpos/spl/spl_progs/os.xsm
 load --int=timer $HOME/myexpos/spl/spl_progs/timerint.xsm
+load --int=console $HOME/myexpos/spl/spl_progs/consoleint.xsm
+load --int=6 $HOME/myexpos/spl/spl_progs/intread.xsm
 load --int=7 $HOME/myexpos/spl/spl_progs/intwrite.xsm
 load --int=10 $HOME/myexpos/spl/spl_progs/intexit.xsm
 load --exhandler $HOME/myexpos/spl/spl_progs/exhandler.xsm
