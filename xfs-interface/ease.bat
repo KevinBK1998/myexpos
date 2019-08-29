1e1627bdbfbf261e2ba80c53f510cdf776a0cc7f
KevinBK <kevinbijoykurian@gmail.com>
2019-08-29 19:47:57 +0530

    Stage 13 completed

---
+++ xfs-interface/ease.bat
@@ -0,0 +1,10 @@
+load --init $HOME/myexpos/expl/samples/num.xsm
+load --idle $HOME/myexpos/expl/samples/idle.xsm
+load  --os $HOME/myexpos/spl/spl_progs/os.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --int=timer $HOME/myexpos/spl/spl_progs/timerint.xsm
+load --int=7 $HOME/myexpos/spl/spl_progs/intwrite.xsm
+load --int=10 $HOME/myexpos/spl/spl_progs/intexit.xsm
+load --exhandler $HOME/myexpos/spl/spl_progs/exhandler.xsm
+load --module 7 ../spl/spl_progs/bootmod.xsm
+exit
