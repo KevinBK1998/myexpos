c39498f46e43851e0ef6cd8ad16c961de55e53d3
KevinBK <kevinbijoykurian@gmail.com>
2019-08-30 21:15:01 +0530

    Stage 17 completed

--- xfs-interface/ease.bat
+++ xfs-interface/ease.bat
@@ -1,12 +1,15 @@
-load --init $HOME/myexpos/expl/samples/bubble.xsm
+load --init $HOME/myexpos/expl/samples/odd.xsm
 load --os $HOME/myexpos/spl/spl_progs/os.xsm
 load --int=timer $HOME/myexpos/spl/spl_progs/timerint.xsm
 load --int=console $HOME/myexpos/spl/spl_progs/consoleint.xsm
 load --int=6 $HOME/myexpos/spl/spl_progs/intread.xsm
 load --int=7 $HOME/myexpos/spl/spl_progs/intwrite.xsm
+load --int=9 $HOME/myexpos/spl/spl_progs/intexec.xsm
 load --int=10 $HOME/myexpos/spl/spl_progs/intexit.xsm
 load --exhandler $HOME/myexpos/spl/spl_progs/exhandler.xsm
 load --module 0 ../spl/spl_progs/resmod.xsm
+load --module 1 ../spl/spl_progs/procmod.xsm
+load --module 2 ../spl/spl_progs/memmod.xsm
 load --module 4 ../spl/spl_progs/devmod.xsm
 load --module 5 ../spl/spl_progs/schedmod.xsm
 load --module 7 ../spl/spl_progs/bootmod.xsm
