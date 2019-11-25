477ea390106aabf93cde076a26554c7c41696503
KevinBK <kevinbijoykurian@gmail.com>
2019-11-26 01:39:28 +0530

    Stage 24 complete

--- xfs-interface/ease.bat
+++ xfs-interface/ease.bat
@@ -4,6 +4,7 @@ load --int=timer $HOME/myexpos/spl/spl_progs/timerint.xsm
 load --int=console $HOME/myexpos/spl/spl_progs/consoleint.xsm
 load --int=disk $HOME/myexpos/spl/spl_progs/diskint.xsm
 load --int=4 $HOME/myexpos/spl/spl_progs/int4.xsm
+load --int=5 $HOME/myexpos/spl/spl_progs/int5.xsm
 load --int=6 $HOME/myexpos/spl/spl_progs/intread.xsm
 load --int=7 $HOME/myexpos/spl/spl_progs/intwrite.xsm
 load --int=8 $HOME/myexpos/spl/spl_progs/intfork.xsm
@@ -17,6 +18,7 @@ load --exhandler $HOME/myexpos/spl/spl_progs/exhandler.xsm
 load --module 0 ../spl/spl_progs/resmod.xsm
 load --module 1 ../spl/spl_progs/procmod.xsm
 load --module 2 ../spl/spl_progs/memmod.xsm
+load --module 3 ../spl/spl_progs/filemod.xsm
 load --module 4 ../spl/spl_progs/devmod.xsm
 load --module 5 ../spl/spl_progs/schedmod.xsm
 load --module 7 ../spl/spl_progs/bootmod.xsm
